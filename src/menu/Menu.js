0bb1fd18d1babf0d636f13c16876d0cb3fa4a64e
brrimolo <bruno.rimolo@gmail.com>
2021-04-07 00:29:10 -0300

    Alterada a NavBar

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -5,24 +5,30 @@ export default class Menu extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-light fixed-top p-4 justify-content-left">
-                <div className="col-12 bg-dark text-white p-2">
+                <div className="col-1">
+                </div>
+                <div className="col-9 bg-dark text-white p-3">
                     <ul class="navbar-nav">
-                        <div>
+                        <div className="col-2">
                             <a><Link className="nav-link" to="/">Página Inicial</Link></a>
                         </div>
-                        <div>
+                        <div className="col-2">
                             <a><Link className="nav-link" to="/cardapio">Cardápio</Link></a>
                         </div>
-                        <div>                        
+                        <div className="col-2">                        
                             <a><Link className="nav-link" to="/reservas">Reservas</Link></a>
                         </div>
-                        <div>
+                        <div className="col-2">
                             <a><Link className="nav-link" to="/carrinho">Carrinho</Link></a>
                         </div>
-                        <div className="justify-content-right">
-                            <a><Link className="nav-link" to="/login">Login</Link></a>
-                        </div>
                     </ul>
+                </div>
+                <div className="col-1 bg-dark text-white p-3">
+                    <div>
+                        <a><Link className="nav-link text-white" to="/login">Login</Link></a>
+                    </div>
+                </div>
+                <div className="col-1">
                 </div>
             </nav>
         )
